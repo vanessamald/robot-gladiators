0ea22e4ebf28d9fdb0ccb28c5ef512e1843f0c84
Vanessa Maldonado <vanessa.rodriguez3@mavs.uta.edu>
2022-03-24 20:00:36 -0500

    updating JS variables

--- js/game.js
+++ js/game.js
@@ -1,16 +1,17 @@
 var playerName = window.prompt("What is your robot's name?");
-// Note the lack of quotation marks around playerName
-console.log(playerName); 
+var playerHealth = 100;
+var playerAttack = 10;
 
-console.log("This logs a string, good for leaving yourself a message");
-// this will do math and log 20
-console.log(10+10);
-// what is this?
-console.log("Our robots's name is " + playerName);
+// You can also log multiple values at once like this 
+console.log(playerName, playerAttack, playerHealth);
 
+var enemyName= "Roborto";
+var enemyHealth= 50;
+var enemyAttack = 12;
 
-function fight() {
-    window.alert("The fight has begun!");
-}
+var fight = function() {
+    window.alert("Welcome to Robot Gladiators");
+};
 
-//fight();
+fight();
+
