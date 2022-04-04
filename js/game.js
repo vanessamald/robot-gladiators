5b8a12f183485c87e34ffe0fff049acd354e7367
Vanessa Maldonado <vanessa.rodriguez3@mavs.uta.edu>
2022-04-04 13:31:58 -0500

    updating shop function to handle and integer input

--- js/game.js
+++ js/game.js
@@ -57,6 +57,7 @@ var fight = function(enemy) {
     
 
 
+
     //if yes(true), leave fight
     if (confirmSkip) {
         window.alert(playerInfo.name + ' has decided to skip this fight. Goodbye!');
@@ -236,21 +237,21 @@ else {
 var shop = function() {
     //ask player what they'd like to do
     var shopOptionPrompt = window.prompt(
-        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL'. 'UPGRADE', or 'LEAVE' to make a choice. "
+        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter 1 for REFILL, 2 for UPGRADE, or 3 for LEAVE. "
     );
+
+// convert answer to a number
+shopOptionPrompt = parseInt(shopOptionPrompt);
 
 // use switch to carry out action
 switch (shopOptionPrompt) {
-    case 'REFILL':
-    case "refill":
+    case 1:
         playerInfo.refillHealth();
         break;
-    case 'UPGRADE':
-    case "upgrade":
+    case 2:
         playerInfo.upgradeAttack();
         break;
-    case "LEAVE": // new case
-    case "leave":
+    case 3:
         window.alert("Leaving the store.");
         break;
     default:
